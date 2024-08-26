6b7a3e3ffdf8f3d77d6105f5a129cbea1673fec7
Ashutosh Sharma <ashutoshsharmaengg@live.com>
2024-08-26 01:49:54 -0700

    constants for data validation updated

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -34,3 +34,8 @@ DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
 """
 Data Validation related constant start with DATA_VALIDATION Var name
 """
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = 'validated'
+DATA_VALIDATION_INVALID_DIR: str = 'invalid'
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
