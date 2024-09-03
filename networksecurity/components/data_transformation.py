6a085f06df675cfdcfee9bdb54cf8fdbaa7d9223
Ashutosh Sharma <ashutoshsharmaengg@live.com>
2024-09-02 19:32:23 -0700

    added read write pickle and numpy array class in utils

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -1,12 +1,17 @@
 import os,sys
 import pandas as pd
 import numpy as np
+from sklearn.impute import KNNImputer
+from sklearn.pipeline import Pipeline
 
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logger.logger import logging
 
 from networksecurity.entity.config_entity import DataTransformationConfig
-from networksecurity.entity.artifact_entity import DataValidationArtifact
+from networksecurity.entity.artifact_entity import DataValidationArtifact, DataTransformationArtifact
+from networksecurity.constant.training_pipeline import TARGET_COLUMN, DATA_TRANSFORMATION_IMPUTER_PARAMS
+from networksecurity.utils.main_utils.utils import save_numpy_array, save_object
+
 
 class DataTransformation:
     def __init__(self):
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -3,6 +3,7 @@ import dill
 import pickle
 import numpy as np
 import yaml
+import pickle
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logger.logger import logging
 
@@ -26,6 +27,47 @@ def write_yaml_file(file_path:str, content:object, replace:bool = False)->None:
         ## open file in write mode and dump the content
         with open(file_path, "w") as file:
             yaml.dump(content,file)
+            
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+def save_numpy_array_data(file_path:str, arr: np.array) -> None:
+    try:
+        ## save numpy array data to file
+        dir_name = os.path.dirname(file_path)
+        os.makedirs(dir_name, exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            np.save(file_obj, arr)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+def load_numpy_array_data(file_path: str):
+    try:
+        ## load numpy array data from file
+        with open(file_path) as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+def save_object(file_path:str, obj:object) -> None:
+    try:
+        ## saving pickle file
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path,'wb') as file_obj:
+            pickle.dump(obj,file_obj)
+        
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+def load_object(file_path:str):
+    try:
+        ## raise error if pickle file doesn't exists
+        if not os.path.exists(file_path):
+            raise NetworkSecurityException(f"The file: {file_path} is not exists")
+        ## load pickle object
+        with open(file_path) as file_obj:
+            return pickle.load(file_obj)
             
     except Exception as e:
         raise NetworkSecurityException(e,sys)
