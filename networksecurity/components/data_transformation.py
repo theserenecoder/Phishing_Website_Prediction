4b78eeddc52f39138fd611fe16809d7277f37888
Ashutosh Sharma <ashutoshsharmaengg@live.com>
2024-09-02 19:56:05 -0700

    data transformation structure defined

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.impute import KNNImputer
 from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
 
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logger.logger import logging
@@ -14,5 +15,29 @@ from networksecurity.utils.main_utils.utils import save_numpy_array, save_object
 
 
 class DataTransformation:
-    def __init__(self):
-        pass
+    def __init__(self, data_validation_artifact: DataValidationArtifact,
+                 data_transformation_config: DataTransformationConfig):
+        try:
+            self.data_transformation_config : DataTransformationConfig = data_transformation_config
+            self.data_validation_artifact = data_validation_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    @staticmethod
+    def read_data(file_path: str)->pd.DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def get_data_transfomer_object(cls)->Pipeline:
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def initiate_data_transformation(self)->DataTransformationArtifact:
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
